a42316cc4884afeaee689b1f05527f66bae3b8a8
Kevin Paternina <kevinpaternina99@gmail.com>
2020-09-01 18:56:49 -0500

    Se agregó el header de la app

--- ClientApp/src/app/components/header/header.component.ts
+++ ClientApp/src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MenubarModule} from 'primeng/menubar';
 import {MenuItem} from 'primeng/api';
+
 
 @Component({
   selector: 'app-header',
@@ -15,26 +17,27 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-          label: 'File',
-          items: [{
-                  label: 'New',
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
+          label: 'INICIO',
       },
       {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-          ]
+          label: 'CONÓCENOS',
+          // icon: 'pi pi-fw pi-pencil',
+          // items: [
+          //     {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+          //     {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+          // ]
+      },
+      {
+        label: 'PUNTOS DE RECOLECCIÓN',
+      },
+      {
+        label: 'FUNDACIONES',
+      },
+      {
+        label: 'VOLUNTARIADO',
+      },
+      {
+        label: 'MI PERFIL',
       }
     ];
   }
