1a55bc9f066d15eb1c7318ab5a418f2469458e42
Yorbi <yorbicardozo2@gmail.com>
2020-09-04 14:09:08 -0500

    Tabs

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  items: MenuItem[];
   title = 'FundacionPuntosVerdesLito';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'MI PERFIL',
+        routerLink: 'profile'
+      },
+      {
+        label: 'SEGURIDAD',
+        routerLink: 'security'
+      },
+      {
+        label: 'FUNDACIONES',
+        routerLink: 'foundations'
+      },
+      {
+        label: 'HISTORIAL',
+        routerLink: 'history'
+      }
+    ];
+
+  }
 }
