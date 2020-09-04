aefec389b24374dea006ff73a5c052d3eb58e54e
Yorbi <yorbicardozo2@gmail.com>
2020-09-04 09:59:25 -0500

    footer updated

--- ClientApp/src/app/components/footer/footer.component.ts
+++ ClientApp/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-footer',
@@ -6,14 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  
+
   year: number;
+  items: MenuItem[];
 
   constructor() {
     this.year = new Date().getFullYear();
    }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        items: [
+          {label: 'Inicio'},
+          {label: 'Conócenos'},
+          {label: 'Recolección'},
+          {label: 'Blog'}
+        ]
+      },
+      {
+        items: [
+          {label: 'Voluntariado'},
+          {label: 'Inicia sesión'}
+        ]
+      },
+      {
+        items: [
+          {label: 'Soy fundación'},
+          {label: 'Solicitar mi recolección'},
+          {label: 'Descargar certificados'},
+          {label: 'Preguntas frecuentes'}
+        ]
+      },
+    ];
   }
 
 }
--- ClientApp/src/app/components/header/header.component.ts
+++ ClientApp/src/app/components/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MenubarModule} from 'primeng/menubar';
-import {MenuItem} from 'primeng/api';
-
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
